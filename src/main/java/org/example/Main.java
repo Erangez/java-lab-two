113b48e394b010c89536d540bfba19a6b35cf618
Erangaz <vitalij.eliseev.06@mail.ru>
2025-09-12 00:57:33 +0300

    test

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main{
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 0; i < 10; i++){
+            number += i * 2;
+        }
+        System.out.println(number);
+    }
+}
