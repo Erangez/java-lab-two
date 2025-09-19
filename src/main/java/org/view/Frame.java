cf9d9d56ad2059d8b37642a20839949f20440de3
Erangaz <vitalij.eliseev.06@mail.ru>
2025-09-19 12:25:12 +0300

    •Подготовлен базовый шаблон для дальнейшей работы

--- src/main/java/org/view/Frame.java
+++ src/main/java/org/view/Frame.java
@@ -5,12 +5,10 @@ import javax.swing.*;
 public class Frame extends JFrame{
     Frame(){
         // Базовое окно
-        ImageIcon icon = new ImageIcon("logo.png");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
         this.setVisible(true);
-        this.setIconImage(icon.getImage());
         this.setTitle("Расчёт ИМТ");
 
         // UI
